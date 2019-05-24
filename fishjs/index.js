78535b10430730d94c8c5f9b56f4ea56ffd938ff
wangcuimei <cuimei.wang.it@163.com>
2019-05-24 18:37:06 +0800

    啊啊

--- fishjs/index.js
+++ fishjs/index.js
@@ -6,19 +6,22 @@ var app = new Vue({
                 id:1,
                 name:"item1",
                 price:200,
-                count:1
+                count:1,
+                checked:true
             },
             {
                 id:2,
                 name:"item2",
                 price:150,
-                count:2
+                count:2,
+                checked:false
             },
             {
                 id:3,
                 name:"item3",
                 price:200,
-                count:2
+                count:2,
+                checked:true
             }
         ]
     },
@@ -28,7 +31,9 @@ var app = new Vue({
             for(var i=0; i<this.list.length; i++)
             {
                 var item = this.list[i];
-                price += item.price* item.count;
+                if(item.checked){
+                    price += item.price* item.count;
+                }
             }
             return price.toString().replace(/\B(?=(\d{3})+$)/g,',');;
         }
@@ -46,6 +51,24 @@ var app = new Vue({
         handleRemove:function(index)
         {
             this.list.splice(index,1);
+        },
+        check:function(index)
+        {
+            this.list[index].checked = !this.list[index].checked;
+        },
+        checkAll: function(e)
+        {
+            //var checkObj = document.querySelectorAll(".checkItem");
+            if(e.target.checked)
+            {
+                this.list.forEach(item=>{
+                    item.checked=true;
+                });
+            }else{
+                this.list.forEach(item=>{
+                    item.checked=false;
+                });
+            }
         }
     }
 });
